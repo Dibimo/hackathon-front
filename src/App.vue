1d6cf77e8ea2c43ff2ea3bf029f6a52e86a2f468
GAMASTI\vitor.santos <vitor.santos@moovefy.com.br>
2023-04-29 16:47:11 -0300

    import quasar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,67 @@
-<script setup lang="ts">
+<script lang="ts">
+import { useQuasar } from 'quasar';
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  data () {
+    
+    return {
+      message: 'Hello World!',
+      name: null,
+      age: null,
+      accept: null
+      
+    }
+  },
+  methods: {
+    teste(){
+      this
+    }
+  },
+  setup(){
+    const $q = useQuasar();
+    function alertCustom () {
+      $q.dialog({
+        title: 'Alert',
+        message: 'Vitin é brabo demais',
+      })
+
+    }
+    return { alertCustom }
+  }
+})
 </script>
 
 <template>
   <div>
-    
+    <!-- <q-form
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="name"
+        label="Your name *"
+        hint="Name and surname"
+        lazy-rules
+      />
+
+      <q-input
+        filled
+        type="number"
+        v-model="age"
+        label="Your age *"
+        lazy-rules
+      />
+
+      <q-toggle v-model="accept" label="I accept the license and terms" />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form> -->
+    <div @click="alertCustom">teste</div>
   </div>
 </template>
 
