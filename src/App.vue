dda2d2a66f64ebd470c14f77916722c5850de299
diogo.moreno <diogo.moreno@moovefy.com.br>
2023-04-29 17:31:29 -0300

    Db json

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
+<script>
 </script>
 
 <template>
   <div>
-    
+
   </div>
 </template>
 
---
+++ src/shared/validacao/RegraValidacao.ts
@@ -0,0 +1,6 @@
+export default class RegraValidacao {
+
+    constructor(){
+
+    }
+}
